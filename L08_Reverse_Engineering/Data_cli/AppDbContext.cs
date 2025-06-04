0b51c4a0c2e6021c8ce3b9f6b4952b81852ccf48
ahmed151515 <ahmedarafa.200444@gmail.com>
2025-06-04 16:15:07 +0300

    L08 ended

---
+++ L08_Reverse_Engineering/Data_cli/AppDbContext.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using L08_Reverse_Engineering.Entities_cli;
+using Microsoft.EntityFrameworkCore;
+
+namespace L08_Reverse_Engineering.Data_cli;
+
+public partial class AppDbContext : DbContext
+{
+    public AppDbContext()
+    {
+    }
+
+    public AppDbContext(DbContextOptions<AppDbContext> options)
+        : base(options)
+    {
+    }
+
+    public virtual DbSet<Event> Events { get; set; }
+
+    public virtual DbSet<Speaker> Speakers { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see https://go.microsoft.com/fwlink/?LinkId=723263.
+        => optionsBuilder.UseSqlServer("Server=.;Database=TechTalk;User=sa;Password=SqlServer1;TrustServerCertificate=True");
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Event>(entity =>
+        {
+            entity.HasKey(e => e.Id).HasName("PK__Events__3214EC07F2E40AB0");
+
+            entity.Property(e => e.EndAt).HasColumnType("datetime");
+            entity.Property(e => e.StartAt).HasColumnType("datetime");
+            entity.Property(e => e.Title).HasMaxLength(255);
+
+            entity.HasOne(d => d.Speaker).WithMany(p => p.Events)
+                .HasForeignKey(d => d.SpeakerId)
+                .OnDelete(DeleteBehavior.ClientSetNull)
+                .HasConstraintName("FK__Events__SpeakerI__267ABA7A");
+        });
+
+        modelBuilder.Entity<Speaker>(entity =>
+        {
+            entity.HasKey(e => e.Id).HasName("PK__Speakers__3214EC07AA0F37AD");
+
+            entity.Property(e => e.FirstName).HasMaxLength(25);
+            entity.Property(e => e.LastName).HasMaxLength(25);
+        });
+
+        OnModelCreatingPartial(modelBuilder);
+    }
+
+    partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+}
---
+++ L08_Reverse_Engineering/Program.cs
@@ -0,0 +1,89 @@
+﻿using CLI = L08_Reverse_Engineering.Data_cli;
+using PMC = L08_Reverse_Engineering.Data;
+
+namespace L08_Reverse_Engineering
+{
+	internal class Program
+	{
+		/*
+		 * How to keep EF-Core Model & DB Schema in sync?
+		 *	Code source of truth
+		 *		migrations 
+		 *		Greenfield Projects
+		 *	DB source of truth
+		 *		Reverse Engineering
+		 *			old projects
+		 *			DB very big
+		 *		Limitations
+		 *			Providers not equal
+		 *			Data Type support/ Provider
+		 *			Concurrency Token in EF-core issue
+		 *		Pre-Requisite
+		 *			PMC tool / dotnet CLI tool
+		 *			Microsoft.EntityFrameworkCore.Design
+		 *			Install Provider for Database Schema
+		*/
+
+
+		static void Main(string[] args)
+		{
+			// Step #1: Package Manager Console (PMC)
+			//    Tools -> Nuget Package Manager -> Package Manager Console
+
+			// Step #2: Package Manager Console (PMC) Tool 
+			//    Install-Package Microsoft.EntityFrameworkCore.Tools
+
+			// Step #3: Install Nuget Page on Project Microsoft.EntityFrameworkCore.Design
+			// Microsoft.EntityFrameworkCore.SqlServer
+
+			// Step #4: Install Provider in the project Microsoft.EntityFrameworkCore.SqlServer
+
+			// Step #5: Run Command (Full)
+			//    Scaffold-DbContext '[Connection String]' [Provider]
+
+			//  Scaffold-DbContext 'Server=.;Database=TechTalk;User=sa;Password=SqlServer1;TrustServerCertificate=True' Microsoft.EntityFrameworkCore.SqlServer -Context AppDbContext -ContextDir Data -OutputDir Entities
+			ReverseEngineering();
+
+			// Step #1: Windows Terminal (Command Prompt) 
+
+			// Step #2: Install Ef-Core tool globally
+			//    dotnet tool install --global dotnet-ef    (new)
+			//    dotnet tool update --global dotnet-ef     (to upgrade)
+
+			// Step #3: Install Provider in the project Microsoft.EntityFrameworkCore.SqlServer
+
+			// Step #4: Run Command (Full)
+			//    dotnet ef dbcontext scaffold '[Connection String]' [Provider]
+			// dotnet ef dbcontext scaffold 'Server=.;Database=TechTalk;User=sa;Password=SqlServer1;TrustServerCertificate=True' Microsoft.EntityFrameworkCore.SqlServer -c AppDbContext --Context-Dir Data -o Entities
+			ReverseEngineeringNETCLI();
+
+
+		}
+
+
+
+		private static void ReverseEngineering()
+		{
+			using var context = new PMC.AppDbContext();
+			foreach (var item in context.Speakers)
+			{
+
+				Console.WriteLine(item.FirstName + " " + item.LastName);
+
+			}
+		}
+
+		private static void ReverseEngineeringNETCLI()
+		{
+			using var context = new CLI.AppDbContext();
+			foreach (var item in context.Speakers)
+			{
+
+				Console.WriteLine(item.FirstName + " " + item.LastName);
+
+			}
+		}
+
+
+	}
+}
